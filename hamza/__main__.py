c16eaa9afc0dcaeeef6b1b5af8b4f32e6106c8ca
Eliaz Bobadilla <ultirequiem@pm.me>
2023-02-11 12:08:04 -0500

    :sparkles: add bunch stuff

--- hamza/__main__.py
+++ hamza/__main__.py
@@ -1,9 +1,14 @@
 import scrapetube
+import pytube
+import random
+import typer
 import json
 import pathlib
 import time
 import subprocess
 import sys
+
+app = typer.Typer()
 
 
 def startfile(path):
@@ -12,9 +17,38 @@ def startfile(path):
     subprocess.call([opener, path])
 
 
+def assure_dirs():
+    pathlib.Path(f"{pathlib.Path.home()}", ".local", "hamza", "videos").mkdir(
+        parents=True, exist_ok=True
+    )
+
+
+def get_videos_data():
+    with open(
+        pathlib.Path(pathlib.Path.home(), ".local", "hamza", "config.json")
+    ) as file:
+        return json.load(file)
+
+
+def get_random_video():
+    return random.choice(get_videos_data())
+
+
+assure_dirs()
+
 HAMZA_CHANNEL_ID = "UCWsslCoN3b_wBaFVWK_ye_A"
 
 
+@app.command()
+def info():
+    video_data = get_videos_data()
+
+    print(f"Videos indexed: {len(video_data)}")
+    print(f"Videos downloaded: 0")
+    print(f"Videos watched: 0")
+
+
+@app.command()
 def setup():
     print("This could take a couple of minutes...")
 
@@ -43,18 +77,44 @@ def setup():
 
             time.sleep(2)
 
-    pathlib.Path(f"{pathlib.Path.home()}", ".local", "hamza").mkdir(
-        parents=True, exist_ok=True
-    )
-
     json_data = json.dumps(video_data, indent=4)
 
     with open(
-        pathlib.Path(f"{pathlib.Path.home()}", ".local", "hamza","config.json"), "w"
+        pathlib.Path(pathlib.Path.home(), ".local", "hamza", "config.json"), "w"
     ) as data_file:
         data_file.write(json_data)
 
     print("Setup complete!")
 
 
-setup()
+@app.command()
+def watch():
+    video = get_random_video()
+
+    vid_path = str(
+        pathlib.Path(
+            pathlib.Path.home(), ".local", "hamza", "videos", f"{video['video_id']}.mp4"
+        )
+    )
+
+    print(f"You are about to watch '{video['video_title']}'.")
+
+    stream = (
+        pytube.YouTube(video["video_url"])
+        .streams.filter(progressive=True, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+    )
+
+    if not stream:
+        return
+
+    stream.download(vid_path)
+
+    typer.launch(vid_path)
+
+    print("Enjoy :)")
+
+
+app()
